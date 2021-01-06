b902b734965eda5843c7af8c6661c38322bfc41c
Alexis <psialedev@gmail.com>
2021-01-06 13:24:29 -0600

    I understood what was happening with the permissions and now my app returns the location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,10 +7,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> getLocation() async {
+  Future<Position> getLocation() async {
+    bool serviceEnabled = true;
+    LocationPermission permission;
+    if (!serviceEnabled) {
+      return Future.error(
+          "location services are Disabled, please turn them on");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.checkPermission();
+    }
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
     print(position);
+    return position;
   }
 
   @override
